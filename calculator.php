04bb55cb40d64f48c7676724de8efc594e587eb8
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-22 23:25:30 -0400

    Changes to be committed: 	modified:   calculator.php

--- calculator.php
+++ calculator.php
@@ -6,16 +6,16 @@
 <body>
   <form>
   <p><label><input type="radio" name="int1" value=1>1</label>
-  <p><label><input type="radio" name="int2" value=2>2</label>
-  <p><label><input type="radio" name="int3" value=3>3</label>
+  <label><input type="radio" name="int2" value=2>2</label>
+  <label><input type="radio" name="int3" value=3>3</label>
   <p><label><input type="radio" name="int4" value=4>4</label>
-  <p><label><input type="radio" name="int5" value=5>5</label>
-  <p><label><input type="radio" name="int6" value=6>6</label>
-  <p><label><input type="radio" name="int7" value=7>7</label>
+  <label><input type="radio" name="int5" value=5>5</label>
+  <label><input type="radio" name="int6" value=6>6</label>
+  <label><input type="radio" name="int7" value=7>7</label>
   <p><label><input type="radio" name="int8" value=8>8</label>
-  <p><label><input type="radio" name="int9" value=9>9</label>
+  <label><input type="radio" name="int9" value=9>9</label>
   <p><label><input type="radio" name="int0" value=0>0</label>
-	<p><button>Make your choice</button>
+	<p><button>Calculate!</button>
   </form>
 <?php
 echo "\t<p>Hello World!</p>\n";
