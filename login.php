a63d9284163c0ca04e454d0db26d4cc4177d561c
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 18:31:59 -0400

    login fix

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head><title>Login</title></head>
 <body>
 <?php
-$user = trim($_POST['user']);
+$user = trim(strtolower($_POST['user']));
 $fname = "/home/brian/file_sharing/users.txt";
 $h = fopen($fname, "r");
 
@@ -15,10 +15,17 @@ while( !feof($h) ){
   }
 }
 fclose($h);
-
-header("Location: new_user_prompt.html");
-exit;
+<p>printf("User %s does not exist. Would you like to create a new user?", htmlentities($user));</p>
+#header("Location: new_user_prompt.html");
+#exit;
 ?>
-
+<p>
+  <form action="new_user.html" method="POST">
+    <input type="submit" value="Yes" />
+  </form>
+  <form action="login.html" method="POST">
+    <input type="submit" value="Take me back" />
+  </form>
+</p>
 </body>
 </html>
