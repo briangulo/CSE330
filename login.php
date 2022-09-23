3ac2c6de65b5ab7af56aef404935066d4b58b76e
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 18:45:30 -0400

    new_user bug fix

--- login.php
+++ login.php
@@ -2,15 +2,7 @@
 <html>
 <head><title>Login</title></head>
 <body>
-  <p>
-    <form action="new_user.html" method="POST">
-      <input type="submit" value="Yes" />
-    </form>
-    <form action="login.html" method="POST">
-      <input type="submit" value="Take me back" />
-    </form>
-  </p>
-
+  
   <?php
   $user = trim(strtolower($_POST['user']));
   $fname = "/home/brian/file_sharing/users.txt";
@@ -33,7 +25,6 @@
     header("Location: new_user_prompt.html");
     exit;
   }
-  #<p>printf("User %s does not exist. Would you like to create a new user?", htmlentities($user));</p>
   ?>
 
 </body>
--- new_user.php
+++ new_user.php
@@ -6,48 +6,48 @@
   $user = trim(strtolower($_POST['user']));
   $fname = "/home/brian/file_sharing/users.txt";
 
-  if (is_writable($fname)) {
-    if (!$fp = fopen($fname, "a")) {
-      <p>printf("Error: cannot open user database!");</p>
-      <form action="new_user.html" method="POST">
-        <p><input type="submit" value="Try again" /></p>
-      </form>
-    }
+  #if (is_writable($fname)) {
+  if (!$fp = fopen($fname, "a")) {
+    <p>printf("Error: cannot open user database!");</p>
+    #<form action="new_user.html" method="POST">
+    #  <p><input type="submit" value="Try again" /></p>
+    #</form>
+    #}
 
     while( !feof($h) ){
     	if ( $user == trim(fgets($h)) ) {
         <p>printf('User %s already exists. Please choose another username.', htmlentities($user));</p>
-        <form action="new_user.html" method="POST">
-          <p><input type="submit" value="OK" /></p>
-        </form>
+        #<form action="new_user.html" method="POST">
+        #  <p><input type="submit" value="OK" /></p>
+        #</form>
       } else {
         continue;
       }
     } for($i=0; $i<count(strlen($user)); $i++){
     	  if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $user)) {
           <p>printf('Error: only numbers and letters are allowed in your username.');</p>
-          <form action="new_user.html" method="POST">
-            <p><input type="submit" value="Try again" /></p>
-          </form>
+          #<form action="new_user.html" method="POST">
+          #  <p><input type="submit" value="Try again" /></p>
+          #</form>
         }
     } if (fwrite($fp, $user) === FALSE) {
         <p>printf("Error: cannot store username!");</p>
-        <form action="new_user.html" method="POST">
-          <p><input type="submit" value="Try again" /></p>
-        </form>
+        #<form action="new_user.html" method="POST">
+        #  <p><input type="submit" value="Try again" /></p>
+        #</form>
     }
 
     <p>printf("Success: new user %s was created!", $user);</p>
     fclose($fp);
-    <form action="interface.html" method="POST">
-      <p><input type="submit" value="Go!" /></p>
-    </form>
+    #<form action="interface.html" method="POST">
+    #  <p><input type="submit" value="Go!" /></p>
+    #</form>
 
   } else {
     <p>printf("Error: the user database is not writable!");</p>
-    <form action="new_user.html" method="POST">
-      <p><input type="submit" value="Try again" /></p>
-    </form>
+    #<form action="new_user.html" method="POST">
+    #  <p><input type="submit" value="Try again" /></p>
+    #</form>
   }
   ?>
 </body>
