4cc491f47f918176b707d905614b4f450743ebc2
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 19:56:11 -0400

    login

--- login.php
+++ login.php
@@ -2,6 +2,23 @@
 <html>
 <head><title>Login</title></head>
 <body>
+  <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+  	<p>
+  		<label for="username">Username:</label>
+  		<input type="text"
+  		name="user"
+  		id="userinput"
+  		minlength='1'
+  		maxlength='10'
+  		required />
+      <input type="submit" value="Login" />
+  	</p>
+  </form>
+  <p>
+  	Would you like to create a new user?
+  	<form action="new_user.html" method="POST">
+  		<input type="submit" value="Yes" />
+  	</form>
 
   <?php
   $user = trim(strtolower($_POST['user']));
