85f0dad0b8ef4097060e20d696449270e230db06
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 18:36:40 -0400

    login fix

--- login.php
+++ login.php
@@ -2,30 +2,32 @@
 <html>
 <head><title>Login</title></head>
 <body>
-<?php
-$user = trim($_POST['user']);
-$fname = "/home/brian/file_sharing/users.txt";
-$h = fopen($fname, "r");
+  <p>
+    <form action="new_user.html" method="POST">
+      <input type="submit" value="Yes" />
+    </form>
+    <form action="login.html" method="POST">
+      <input type="submit" value="Take me back" />
+    </form>
+  </p>
+  
+  <?php
+  $user = trim(strtolower($_POST['user']));
+  $fname = "/home/brian/file_sharing/users.txt";
+  $h = fopen($fname, "r");
 
-while( !feof($h) ){
-	if ( $user === trim(fgets($h)) ) {
-    header("Location: interface.html");
-  } else {
-    continue;
+  while( !feof($h) ){
+  	if ( $user == trim(fgets($h)) ) {
+      header("Location: interface.html");
+    } else {
+      continue;
+    }
   }
-}
-fclose($h);
-<p>printf("User %s does not exist. Would you like to create a new user?", htmlentities($user));</p>
-#header("Location: new_user_prompt.html");
-#exit;
-?>
-<p>
-  <form action="new_user.html" method="POST">
-    <input type="submit" value="Yes" />
-  </form>
-  <form action="login.html" method="POST">
-    <input type="submit" value="Take me back" />
-  </form>
-</p>
+  fclose($h);
+  <p>printf("User %s does not exist. Would you like to create a new user?", htmlentities($user));</p>
+  #header("Location: new_user_prompt.html");
+  #exit;
+  ?>
+
 </body>
 </html>
