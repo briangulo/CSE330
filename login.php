0e6272ec4430ce7fcca4e8e2d74aa75bc4a7cd46
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 17:01:28 -0400

    fixed user check

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $user = $_POST['user'];
 $h = fopen("/home/brian/file_sharing/users.txt", "r");
 
 while( !feof($h) ){
-	if ( $user = trim(fgets($h)) ) {
+	if ( $user == trim(fgets($h)) ) {
     header("Location: interface.html");
   } else {
     continue;
