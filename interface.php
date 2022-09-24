54e80ebc4126ae8fb8bfdd6191b87239370524da
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 20:21:15 -0400

    colons

--- interface.php
+++ interface.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My First Web Page</title>
+	<title>Files</title>
 </head>
 <body>
 	<?php
 	printf("<p>Hello %s!</p>", @$_SESSION['user']);
-  echo "<form action=\"login.php\" method=\"POST\">"
-  echo "<input type=\"submit\" value=\"Take me back\" />"
-  echo "</form>"
+  echo "<form action=\"login.php\" method=\"POST\">";
+  echo "<input type=\"submit\" value=\"Take me back\" />";
+  echo "</form>";
 	?>
 </body>
 </html>
