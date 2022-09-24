1429ca87ce88720c709e79b942411b802ee76d09
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-24 08:31:51 -0400

    colon

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ require "generate_path.php";
 
 $_SESSION['file'] = basename($_FILES['uploadedfile']['name']);
 $full_path = generate_path();
-$_SESSION['full_path'] = $full_path
+$_SESSION['full_path'] = $full_path;
 
 if( move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $full_path) ){
 	header("Location: upload_success.html");
