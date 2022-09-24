5a32d5ea32b748d6f99d4dff1772e6a1b51b53dd
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 22:34:30 -0400

    file sharing

--- interface.php
+++ interface.php
@@ -7,9 +7,35 @@
 	<?php
 	session_start();
 	$user = $_SESSION['user'];
+	$userdir = '/home/brian/file_sharing/'.$user
 
-	printf("<p>Hello %s!</p>", htmlentities($user));
+	printf("<p>Welcome <strong>%s!</strong></p>", htmlentities($user));
 
+	$num = 1
+	foreach(glob($userdir.'/*.*') as $file) {
+    printf("<p>%d: %s</p>", $num, $file);
+	}
+/***
+	$filename = $_GET['file'];
+
+	// We need to make sure that the filename is in a valid format; if it's not, display an error and leave the script.
+	// To perform the check, we will use a regular expression.
+	if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
+		echo "Invalid filename";
+		exit;
+	}
+
+	$full_path = sprintf("/srv/uploads/%s/%s", $username, $filename);
+
+	// Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
+	$finfo = new finfo(FILEINFO_MIME_TYPE);
+	$mime = $finfo->file($full_path);
+
+	// Finally, set the Content-Type header to the MIME type of the file, and display the file.
+	header("Content-Type: ".$mime);
+	header('content-disposition: inline; filename="'.$filename.'";');
+	readfile($full_path);
+	***/
   echo "<form action=\"login.php\" method=\"POST\">";
   echo "<input type=\"submit\" value=\"Logout\" />";
   echo "</form>";
--- login.php
+++ login.php
@@ -24,9 +24,9 @@
 
   <?php
   if ( isset($_POST["user"]) ) {
-    
+
     if( isset($_SESSION["user"]) ){
-      unset($_SESSION["user"]);
+      session_destroy();
     }
 
     session_start();
