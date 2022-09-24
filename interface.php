5e0abfe3ff73ec45c6f092fbb428b947e44aa10a
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-24 14:27:29 -0400

    changes

--- interface.php
+++ interface.php
@@ -15,7 +15,8 @@
 	$num = 1;
 	foreach($scan as $file) {
 		if (!is_dir($file)) {
-			printf("<p>%d: %s</p>", $num, htmlentities($file));
+			$tmp = $file;
+			printf("<p>%d: %s</p>", $num, htmlentities($tmp));
 			echo "<form action=\"download.php\" method=\"POST\">";
 		  echo "<input type=\"submit\" name=\"$file\" value=\"View\" />";
 		  echo "</form>";
