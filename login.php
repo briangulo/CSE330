8c6b879aa1ff55e19640ec8f591dfaff6f02ae27
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-23 20:01:17 -0400

    login update

--- login.php
+++ login.php
@@ -21,34 +21,37 @@
   	</form>
 
   <?php
-  $user = trim(strtolower($_POST['user']));
-  $fname = "/home/brian/file_sharing/users.txt";
-  $h = fopen($fname, "r");
-  $exists = FALSE;
+  if ( isset($_POST['user']) ) {
+    $user = trim(strtolower($_POST['user']));
+    $fname = "/home/brian/file_sharing/users.txt";
+    $h = fopen($fname, "r");
+    $exists = FALSE;
 
-  while( !feof($h) ){
-  	if ( $user == trim(fgets($h)) ) {
-      $exists = TRUE;
+    while( !feof($h) ){
+    	if ( $user == trim(fgets($h)) ) {
+        $exists = TRUE;
+      } else {
+        continue;
+      }
+    }
+    fclose($h);
+
+    if ($exists) {
+      header("Location: interface.html");
+      exit;
     } else {
-      continue;
+      printf("<p>User %s does not exist. Would you like to create a new user?</p>",
+      htmlentities($user));
+      echo "<form action=\"new_user.html\" method=\"POST\">";
+      echo "<p><input type=\"submit\" value=\"Yes\" /></p>";
+      echo "</form>";
+      echo "<form action=\"login.html\" method=\"POST\">";
+      echo "<p><input type=\"submit\" value=\"Take me back\" /></p>";
+      echo "</form>";
+      exit;
     }
   }
-  fclose($h);
 
-  if ($exists) {
-    header("Location: interface.html");
-    exit;
-  } else {
-    printf("<p>User %s does not exist. Would you like to create a new user?</p>",
-    htmlentities($user));
-    echo "<form action=\"new_user.html\" method=\"POST\">";
-    echo "<p><input type=\"submit\" value=\"Yes\" /></p>";
-    echo "</form>";
-    echo "<form action=\"login.html\" method=\"POST\">";
-    echo "<p><input type=\"submit\" value=\"Take me back\" /></p>";
-    echo "</form>";
-    exit;
-  }
   ?>
 
 </body>
