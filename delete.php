b241c34a66b4a40d01c370fb5f42551b0d5dd97a
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-24 12:45:14 -0400

    changes

--- delete.php
+++ delete.php
@@ -3,6 +3,9 @@
   foreach ($_POST as $key) {
     echo "$key";
   }
+  echo '<form action="interface.php" method="POST">'
+  echo '<p><input type="submit" value="Take me back" /></p>'
+  echo '</form>'
   /***
   if (unlink('/home/tutsplus/files/tmp.txt')) {
     // file was successfully deleted
