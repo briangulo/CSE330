f080635af6b4c013a5850c53d42786251a03dc40
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-26 16:50:45 -0400

    upload debug

--- generate_path.php
+++ generate_path.php
@@ -6,9 +6,8 @@
   session_start();
   function generate_path(){
     $user = $_SESSION['user'];
-    #$file = $_SESSION['file'];
 
-    if ($_SESSION['action'] == 'Delete' or $_SESSION['action'] == 'View') {
+    if ($_SESSION['action'] == "Delete" or $_SESSION['action'] == "View") {
       $files = array_values(array_filter(glob($_SESSION['userdir']."/*"), 'is_file'));
       $file = $files[$_SESSION['file_num']-1];
       $filename = pathinfo($file, PATHINFO_FILENAME);
@@ -19,7 +18,7 @@
 
     if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
     	echo "Invalid filename";
-      #printf("file: %s", $_SESSION['key']);
+      printf("file: %s", $file);
       echo '<form action="interface.php" method="POST">';
       echo '<p><input type="submit" value="Take me back" /></p>';
       echo '</form>';
