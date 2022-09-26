1746b2154eee407a8bd7dba3293c4c566eafebd0
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-26 17:14:35 -0400

    view

--- download.php
+++ download.php
@@ -2,5 +2,14 @@
   session_start();
   require "generate_path.php";
 
-  $_SESSION['file'] = $_POST[];
+  foreach ($_POST as $key => $value) {
+    $_SESSION['file_num'] = $key;
+    $_SESSION['action'] = $value;
+  }
+
+  generate_path();
+  chmod($_SESSION['full_path'], 0757);
+
+  $contents = file_get_contents($_SESSION['full_path']);
+  echo $contents;
 ?>
