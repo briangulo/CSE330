c355ca3788a3cb20fb9fa898fcd84da6b1548fa6
Brian Augusto Angulo Flores <36615035+briangulo@users.noreply.github.com>
2022-09-26 17:18:26 -0400

    view improvements

--- download.php
+++ download.php
@@ -10,10 +10,13 @@
   generate_path();
   chmod($_SESSION['full_path'], 0757);
 
-
   if (file_get_contents($_SESSION['full_path']) === TRUE) {
     $contents = file_get_contents($_SESSION['full_path']);
     echo urlencode($contents);
+    echo '<form action="interface.php" method="POST">';
+    echo '<p><input type="submit" value="Take me back" /></p>';
+    echo '</form>';
+    exit;
   } else {
     header("Location: action_failure.php");
     exit;
